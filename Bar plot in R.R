26f126e03ee133c82448de3799ff33c57e1aab10
Umme Ohida Rahman <ohida.mfs@bau.edu.bd>
2025-09-10 22:55:54 +0600

    oneway.anova part 2

---
+++ Bar plot in R.R
@@ -0,0 +1,43 @@
+# Creating a data frame
+dataset = data.frame(dose = c("D0.5", "D1", "D2"), 
+                     len = c(4.2, 10, 29.5),
+                     gsg = c(45, 77, 7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Basic barplot
+Horizontal bar plot
+Changing the width of bars
+Changing colors
+Minimal theme + blue fill color
+Bar plot with labels
+Barplot of counts
